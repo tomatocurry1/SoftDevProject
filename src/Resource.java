d29ebd8feb41cec1749ed0539c20d90d61a13ece
kalmcrantz <kaa97@case.edu>
2017-03-07 17:18:19 -0500

    Add Resource class

---
+++ src/Resource.java
@@ -0,0 +1,27 @@
+
+public class Resource {
+
+	private int price;
+	private Player owner;
+	
+	public Resource(String resourceType) {
+		if (resourceType.equals("Steel"))
+			price = 100;
+		else if (resourceType.equals("Oil"))
+			price = 200;
+		else
+			throw new IllegalArgumentException("Must enter 'Steel' or 'Oil'");
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setOwner(Player p) {
+		owner = p;
+	}
+	
+	public Player getPlayer() {
+		return owner;
+	}
+}
