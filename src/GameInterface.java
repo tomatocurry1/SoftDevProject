0682e650f1163c8099906a16520ba205f17c56ec
tomatocurry1 <Thomas@172.20.44.168>
2017-03-10 07:26:41 -0500

    Added the individual grid rendering

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -2,6 +2,26 @@
 public class GameInterface {
 	
 	public static Tile[][] grid;
+	private static Player plst[] = new Player[4];
+	
+	public static void init(){
+		
+		plst[0] = new Player();
+		
+		grid = new Tile[10][14];
+		
+		for(int i=0; i<10;i++){
+			for(int j=0; j<14;j++){
+				grid[i][j] = new Tile(Terrain.GRASSLANDS, null);
+				if((j==5&&i<5)||(j==6&&i>=5)){
+					grid[i][j].setTerrain(Terrain.WATER);
+				}
+
+			}
+		}
+		grid[2][4].setUnit(new Unit(plst[0]));
+		
+	}
 	
 	void loop(){
 		
