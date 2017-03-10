b51a4d01bff50e86e3d35235c9caaa331d00c676
Dman42997 <Dman42997@gmail.com>
2017-03-10 09:40:30 -0500

    Fixed Tile

--- src/Tile.java
+++ src/Tile.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-
-	public class Tile {
+public class Tile {
 	
 	Terrain terrain;
 	Resource resource;
@@ -33,45 +31,3 @@
 		resource = r;
 	}
 }
-
-=======
-
-	public class Tile {
-	
-	Terrain terrain;
-	Resource resource;
-	Unit unit;
-	City building;
-	
-	public Tile (Terrain t, Resource r) {  // resource can be either a traditional resource or a building
-		this.setTerrain(t);
-		this.setResource(r);
-	}
-	
-	public Resource getResource() {
-		return resource;
-	}
-	
-	public void setResource(Resource r) {
-		resource = r;
-	}
-	
-	
-	public Terrain getTerrain() {
-		return  terrain;
-	}
-	
-	public Unit getUnit() {
-		return unit;
-	}
-	
-	public void setUnit(Unit u) {
-		unit = u;
-	}
-	
-	public void setTerrain(Terrain t) {
-		terrain = t;
-	}
-}
-
->>>>>>> branch 'master' of https://github.com/tomatocurry1/SoftDevProject/
