dd8bcc84a6cf1b481deae099f841fc35d2598f51
kalmcrantz <kaa97@case.edu>
2017-03-10 14:05:41 -0500

    Prints Terrain

--- src/Terrain.java
+++ src/Terrain.java
@@ -1,20 +1,27 @@
 
 public enum Terrain {
 
-	GRASSLANDS(1.0),
-	HILLS(1.5),
-	MOUNTAINS(2.0),
-	ROADS(0.5),
-	WATER(1.0);
+	GRASSLANDS(1.0, "Grasslands"),
+	HILLS(1.5, "Hills"),
+	MOUNTAINS(2.0, "Mountains"),
+	ROADS(0.5, "Roads"),
+	WATER(1.0, "Water");
 	
 	private double modifier;
 	
-	private Terrain (double value) {
+	private String name;
+	
+	private Terrain (double value, String s) {
 		modifier = value;
+		name = s;
 	}
 	
 	public double getModifier() {
 		return modifier;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 }
