dca8b27bd32340e5498d30d5675f29a2404abdb6
kalmcrantz <kaa97@case.edu>
2017-03-30 16:03:47 -0400

    commented out change of resource immediately

--- src/UnitManager.java
+++ src/UnitManager.java
@@ -14,12 +14,13 @@ public class UnitManager {
 
 	public static void moveUnit(Tile tile1, Tile tile2) {
 		Unit temp = tile1.getUnit();
+		//deducts movement points
 		temp.setMovementPts(temp.getMovementPts() - (Math.abs(tile2.getX() - tile1.getX()) + Math.abs(tile2.getY() - tile1.getY())));
 		tile1.setUnit(null);	
 		tile2.setUnit(temp);
 		Player p = tile2.getUnit().getOwner();
-		//if tile has a resource
-		if (tile2.getResource() != null)  {
+		//if tile has a resource, gives ownership to the current player and deducts from previous owner (if applicable)
+		/*if (tile2.getResource() != null)  {
 			Resource r = tile2.getResource();
 			if (r.toString().equals("Steel")) {
 				if (r.getOwner() != null)
@@ -32,7 +33,7 @@ public class UnitManager {
 				p.setOil(p.getOil() + 1);
 			}
 			tile2.getResource().setOwner(p);
-		}
+		}*/
 		if (tile2.getBuilding() != null)
 			tile2.getBuilding().setOwner(p);
 	}
@@ -40,8 +41,10 @@ public class UnitManager {
 	public static void attack(Tile tile1, Tile tile2) {
 		Unit unit1 = tile1.getUnit();
 		Unit unit2 = tile2.getUnit();
+		//checks if there are enough movement pts
 		isMoveValid(tile1, tile2);
 		unit2.decreaseHealth(unit1.getAttack());
+		//if unit is destroyed
 		if (unit2.getHealth() <= 0) {
 			moveUnit(tile1, tile2);
 		}
